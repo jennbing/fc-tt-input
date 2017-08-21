04d360e70837e13619a20bc1d251a3d82d9303e8
jennbing <jennbingong87@gmail.com>
2017-08-22 02:07:57 +0800

    Create setup.m

---
+++ setup.m
@@ -0,0 +1,15 @@
+cd TT-Toolbox-master
+setup
+cd ..
+cd matconvnet-1.0-beta24/matlab
+vl_setupnn
+cd ..
+cd ..
+cd TensorNet-master/src/matlab
+addpath(pwd)
+cd ..
+cd ..
+cd ..
+cd autonn-master
+setup_autonn
+cd ..
